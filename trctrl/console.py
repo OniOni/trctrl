045121cd20f138d331b9bd792fb7364aedf95b4e
mathieu <mathieu.c.sabourin@gmail.com>
2016-08-17 21:56:18 -0700

    Add a couple commands (pause/start/add) + upgrade list.

--- trctrl/console.py
+++ trctrl/console.py
@@ -2,23 +2,47 @@ import argparse
 
 from .lib import commands
 
+def pprint(obj):
+    if isinstance(obj, list):
+        return "\n".join([pprint(e) for e in obj])
+    elif isinstance(obj, dict):
+        return ", ".join(["{}: {}".format(k,v) for k,v in obj.items()])
+
 def setup():
     parser = argparse.ArgumentParser("Transmission ctrl.")
 
     subparsers = parser.add_subparsers(dest='cmd')
 
     ls = subparsers.add_parser('list', help="List torrents")
+    ls.add_argument('-s', '--status', action='store', default='all')
+
     show = subparsers.add_parser('show', help="Show information about specific torrent.")
     add = subparsers.add_parser('add', help="Add torrent to list.")
+    add.add_argument('torrent')
+
     pause = subparsers.add_parser('pause', help="Pause torrent.")
+    pause.add_argument('identifier')
+
+    start = subparsers.add_parser('start', help="Pause torrent.")
+    start.add_argument('identifier')
+
     delete = subparsers.add_parser('delete', help="Delete torrent.")
     update = subparsers.add_parser('update', help="Update torrent.")
 
     return parser.parse_args()
 
 def run(args):
+    res = None
     if args.cmd == 'list':
-        print(commands.list())
+        res = pprint(commands.list_torrents(args.status))
+    elif args.cmd == 'pause':
+        commands.pause_torrents(args.identifier)
+    elif args.cmd == 'start':
+        commands.start_torrents(args.identifier)
+    elif args.cmd == 'add':
+        res = pprint(commands.add_torrent(args.torrent))
+
+    print(res)
 
 if __name__ == '__main__':
     args = setup()
--- trctrl/lib/commands.py
+++ trctrl/lib/commands.py
@@ -1,14 +1,71 @@
+from collections import OrderedDict
 from . import rpc
 
 
-def list(*a, **k):
+def list_torrents(status):
     c = rpc.TrClient()
-    return "\n".join([
-        "id: {}, name: {}, status: {}".format(
-            t['id'], t['name'], t['status']
-        ) for t in c.send(
-            'torrent-get', {
-                'fields': ['id', 'name', 'status']
-            }
-        ).json()['arguments']['torrents']
-    ])
+    res = c.send('torrent-get', {'fields': ['id', 'name', 'status', 'hashString']}).json()
+
+    _all = False
+    if status == 'all':
+        _all = True
+    else:
+        code = {
+            'running': 4,
+            'seeding': 6,
+            'stopped': 0
+        }[status]
+
+    return [
+        OrderedDict([
+            ("id", t['id']),
+            ("name", t['name']),
+            ("status", t['status']),
+            ("hash", t['hashString'])
+        ]) for t in res['arguments']['torrents']
+        if _all or t['status'] == code
+    ]
+
+def pause_torrents(ids):
+    c = rpc.TrClient()
+
+    if ids == 'all':
+        args = None
+    else:
+        args = {'ids': ids}
+
+    res = c.send('torrent-stop', args)
+
+    return res
+
+def start_torrents(ids):
+    c = rpc.TrClient()
+
+    if ids == 'all':
+        args = None
+    else:
+        args = {'ids': ids}
+
+    res = c.send('torrent-start', args)
+
+    return res
+
+def add_torrent(uri):
+    c = rpc.TrClient()
+
+    res = c.send('torrent-add', {
+        'filename': uri
+    }).json()
+
+    print(res)
+
+    if res['result'] == 'success' and 'torrent-added' in res['arguments']:
+        t = res['arguments']['torrent-added']
+        ret = [
+            "Added torrent:",
+            OrderedDict([('name', t['name'])])
+        ]
+    else:
+        ret = ['???']
+
+    return ret
